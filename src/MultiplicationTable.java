5303a0d571c220e3693621130404f95a7eb6b352
Malgorzata Talaga <malgorzatatalaga32@gmail.com>
2023-06-27 17:06:48 +0200

    code refactoring

--- src/MultiplicationTable.java
+++ src/MultiplicationTable.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class MultiplicationTable {
+public class MultiplicationTable {
+    public static int[][] multiplicationTable(int n) {
+        int[][] multiplicationTable = new int[n][n];
+        for (int i = 1; i <= n; i++) {
+            for (int a = 1; a <= n; a++) {
+                multiplicationTable[i - 1][a - 1] = i * a;
+            }
+        }
+        for (int[] x : multiplicationTable) {
+            System.out.println(" ");
+            for (int y : x) {
+                System.out.print(y);
+            }
+
+        }
+        return multiplicationTable;
+    }
 }
