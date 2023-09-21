69c8c4d7899ead40fd5f3b7bcb5385a19aa6995a
Malgorzata Talaga <malgorzatatalaga32@gmail.com>
2023-09-21 17:58:55 +0200

    67. Add Binary - leetcode problem solved

---
+++ src/main/problems/easy/algorithms/AddBinary.java
@@ -0,0 +1,23 @@
+package problems.easy.algorithms;
+
+public class AddBinary {
+    public String addBinary67(String a, String b) {
+        StringBuilder stringBuilder = new StringBuilder();
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0) {
+            int sum = carry;
+            if (i >= 0) sum += a.charAt(i) - '0';
+            if (j >= 0) sum += b.charAt(j) - '0';
+            stringBuilder.append(sum % 2);
+            carry = sum / 2;
+
+            i--;
+            j--;
+            if (carry != 0) stringBuilder.append(carry);
+        }
+        return stringBuilder.reverse().toString();
+    }
+}
