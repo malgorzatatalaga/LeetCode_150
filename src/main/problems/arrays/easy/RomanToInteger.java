d5782e592d9d2df8c8296e2e7275e49ceab9ee43
Malgorzata Talaga <malgorzatatalaga32@gmail.com>
2023-10-03 19:46:27 +0200

    13. Roman to Integer - leetcode problem solved

---
+++ src/main/problems/arrays/easy/RomanToInteger.java
@@ -0,0 +1,31 @@
+package problems.arrays.easy;
+
+/*
+13. Roman to Integer
+ */
+public class RomanToInteger {
+    public int romanToInt(String s) {
+        int answer = 0;
+        int number = 0;
+        int prev = 0;
+
+        for (int j = s.length() - 1; j >= 0; j--) {
+            switch (s.charAt(j)) {
+                case 'M' -> number = 1000;
+                case 'D' -> number = 500;
+                case 'C' -> number = 100;
+                case 'L' -> number = 50;
+                case 'X' -> number = 10;
+                case 'V' -> number = 5;
+                case 'I' -> number = 1;
+            }
+            if (number < prev) {
+                answer -= number;
+            } else {
+                answer += number;
+            }
+            prev = number;
+        }
+        return answer;
+    }
+}
